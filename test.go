862ec7e6248e3f73c317c49628d705037a92bbb6
Richard Conway <richard.conway@telespazio.com>
2019-11-13 17:37:29 +0000

    Code test.go as a long-running service.

--- test.go
+++ test.go
@@ -1,7 +1,18 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	log.Println("Testing build/execution of go program in docker container")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
