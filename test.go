99c0cb63f1d0ab7813df1990590f10e04085f941
Richard Conway <richard.conway@telespazio.com>
2019-11-22 11:34:19 +0000

    Added better json response encoding.

--- test.go
+++ test.go
@@ -1,18 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
+// DingResponse response to a /ding request
+type DingResponse struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
 func main() {
 	log.Println("Testing build/execution of go program in docker container")
 	r := gin.Default()
+
+	// ping
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	// ding
+	r.GET("/ding", func(c *gin.Context) {
+		jsonStr := `
+		{
+			"name": "harry",
+			"age": 26
+		}`
+		dingResponse := DingResponse{}
+		if err := json.Unmarshal([]byte(jsonStr), &dingResponse); err == nil {
+			json.NewEncoder(c.Writer).Encode(dingResponse)
+		} else {
+			log.Panic(err)
+		}
+	})
+
 	r.Run()
 }
